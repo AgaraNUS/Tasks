135c5671ba672b9ba64311aed97decb9757efe93
AgaraNUS <e1384320@u.nus.edu>
2026-02-20 17:51:32 +0800

    Added printDeadlinesUsingStreams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -6,6 +6,8 @@ import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -45,6 +47,13 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static ArrayList<Task> printDeadlinesUsingStreams(ArrayList<Task> tasks, String filterString) {
+        System.out.println("Printing filtered list using streams... ");
+        return (ArrayList<Task>) tasks.stream()
+                .filter(t -> t .getDescription().contains(filterString))
+                .collect(toList());
     }
 
 }
